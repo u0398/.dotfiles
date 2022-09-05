6774a6342c9c5cc18eea2cc9db6405196bc9cdc5
u0398 <ux0398@gmail.com>
2022-09-05 12:14:23 -0700

    Added config setup.

---
+++ .config/nvim/lua/user/persisted.lua
@@ -0,0 +1,25 @@
+local status_ok, persisted = pcall(require, "persisted")
+if not status_ok then
+  return
+end
+
+persisted.setup {
+  save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"), -- directory where session files are saved
+  command = "VimLeavePre", -- the autocommand for which the session is saved
+  use_git_branch = true, -- create session files based on the branch of the git enabled repository
+  branch_separator = "_", -- string used to separate session directory name from branch name
+  autosave = true, -- automatically save session files when exiting Neovim
+  autoload = true, -- automatically load the session for the cwd on Neovim startup
+  on_autoload_no_session = nil, -- function to run when `autoload = true` but there is no session to load
+  allowed_dirs = nil, -- table of dirs that the plugin will auto-save and auto-load from
+  ignored_dirs = nil, -- table of dirs that are ignored when auto-saving and auto-loading
+  before_save = nil, -- function to run before the session is saved to disk
+  after_save = nil, -- function to run after the session is saved to disk
+  after_source = nil, -- function to run after the session is sourced
+  telescope = { -- options for the telescope extension
+    before_source = nil, -- function to run before the session is sourced via telescope
+    after_source = nil, -- function to run after the session is sourced via telescope
+  },
+}
+
+
