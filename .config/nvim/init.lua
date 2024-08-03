31c207f1d587c749e15e69e667aa1c7bb79c8746
u0398 <ux0398@gmail.com>
2024-08-02 21:28:06 -0700

    ibl setup with rainbow colors that match catppuccin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -594,6 +594,69 @@ if statuscol_loaded then
   }
 end
 
+-- lukas-reineke/indent-blankline.nvim {{{2
+-- adds indentation guides to Neovim
+local ibl_loaded, ibl = pcall(require, 'ibl')
+if ibl_loaded then
+  local highlight_bright = {
+    "ibl_bright_red",
+    "ibl_bright_peach",
+    "ibl_bright_yellow",
+    "ibl_bright_green",
+    "ibl_bright_teal",
+    "ibl_bright_sapphire",
+    "ibl_bright_lavender",
+  }
+  local highlight = {
+    "ibl_red",
+    "ibl_peach",
+    "ibl_yellow",
+    "ibl_green",
+    "ibl_teal",
+    "ibl_sapphire",
+    "ibl_lavender",
+  }
+
+  local hooks = require "ibl.hooks"
+  -- create the highlight groups in the highlight setup hook, so they are reset
+  -- every time the colorscheme changes
+  hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "ibl_bright_red", { fg = "#9d6171" })
+    vim.api.nvim_set_hl(0, "ibl_bright_peach", { fg = "#a17563" })
+    vim.api.nvim_set_hl(0, "ibl_bright_yellow", { fg = "#9d8f77" })
+    vim.api.nvim_set_hl(0, "ibl_bright_green", { fg = "#729271" })
+    vim.api.nvim_set_hl(0, "ibl_bright_teal", { fg = "#628f90" })
+    vim.api.nvim_set_hl(0, "ibl_bright_sapphire", { fg = "#5985a0" })
+    vim.api.nvim_set_hl(0, "ibl_bright_lavender", { fg = "#7c81ac" })
+    vim.api.nvim_set_hl(0, "ibl_red", { fg = "#604455" })
+    vim.api.nvim_set_hl(0, "ibl_peach", { fg = "#624e4f" })
+    vim.api.nvim_set_hl(0, "ibl_yellow", { fg = "#605b58" })
+    vim.api.nvim_set_hl(0, "ibl_green", { fg = "#4b5c55" })
+    vim.api.nvim_set_hl(0, "ibl_teal", { fg = "#435b65" })
+    vim.api.nvim_set_hl(0, "ibl_sapphire", { fg = "#3f566d" })
+    vim.api.nvim_set_hl(0, "ibl_lavender", { fg = "#505473" })
+  end)
+
+  ibl.setup {
+    indent = {
+      highlight = highlight,
+      char = "│",
+    },
+    debounce = 100,
+    whitespace = {
+      highlight = highlight,
+      remove_blankline_trail = false,
+    },
+    scope = {
+      enabled = true,
+      char = '│',
+      show_start = false,
+      show_end = false,
+      highlight = highlight_bright,
+    },
+  }
+end
+
 -- echasnovski/mini.nvim (mini-animate) {{{2
 -- animate common Neovim actions
 -- local mini_animate_loaded, mini_animate = pcall(require, 'mini.animate')
