019594c1afd243dc6a54de55aed082a296b55bb9
u0398 <ux0398@gmail.com>
2024-08-02 21:33:11 -0700

    not functioning
    If this can be made to work again, it would make window resizing better.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2149,27 +2149,27 @@ end
 -- can get confusing as left might actually be right, etc
 -- the below can be mapped to arrows and will work similar to the tmux binds
 -- map to: "<cmd>lua require'utils'.resize(false, -5)<CR>"
-local function relative_resize(vertical, margin)
-  local cur_win = vim.api.nvim_get_current_win()
-  -- go (possibly) right
-  vim.cmd(string.format('wincmd %s', vertical and 'l' or 'j'))
-  local new_win = vim.api.nvim_get_current_win()
-  -- determine direction cond on increase and existing right-hand buffer
-  local not_last = not (cur_win == new_win)
-  local sign = margin > 0
-  -- go to previous window if required otherwise flip sign
-  if not_last == true then
-    vim.cmd [[wincmd p]]
-
-  else
-    sign = not sign
-  end
-
-  local sign_char = sign and '+' or '-'
-  local dir = vertical and 'vertical ' or ''
-  local cmd = dir .. 'resize ' .. sign_char .. math.abs(margin) .. '<CR>'
-  vim.cmd(cmd)
-end
+-- local function relative_resize(vertical, margin)
+--   local cur_win = vim.api.nvim_get_current_win()
+--   -- go (possibly) right
+--   vim.cmd(string.format('wincmd %s', vertical and 'l' or 'j'))
+--   local new_win = vim.api.nvim_get_current_win()
+--   -- determine direction cond on increase and existing right-hand buffer
+--   local not_last = not (cur_win == new_win)
+--   local sign = margin > 0
+--   -- go to previous window if required otherwise flip sign
+--   if not_last == true then
+--     vim.cmd [[wincmd p]]
+--
+--   else
+--     sign = not sign
+--   end
+--
+--   local sign_char = sign and '+' or '-'
+--   local dir = vertical and 'vertical ' or ''
+--   local cmd = dir .. 'resize ' .. sign_char .. math.abs(margin) .. '<CR>'
+--   vim.cmd(cmd)
+-- end
 
 -- sudo execution
 local function sudo_exec(cmd, print_output)
