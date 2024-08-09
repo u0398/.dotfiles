c01f6c465a676be6f4eae582aa6bd017fa5d30a4
u0398 <ux0398@gmail.com>
2024-08-09 13:20:41 -0700

    all the [] and toggling should be defined by this plugin

--- .config/nvim/lua/plugins/nvim-impairative.lua
+++ .config/nvim/lua/plugins/nvim-impairative.lua
@@ -119,154 +119,165 @@ return {
             forward = 'blast',
           }
           h:command_pair {
+            key = '<c-b>',
+            backward = 'BufferMoveNext',
+            forward = 'BufferMovePrevious',
+          }
+          h:function_pair {
+            key = 'd',
+            desc = 'Jump to the {previous|next} diagnositic in the current buffer',
+            backward = function() vim.diagnostic.jump( { count = -1, float = true } ) end,
+            forward = function() vim.diagnostic.jump( { count = 1, float = true } ) end,
+          }
+          h:command_pair {
             key = 'l',
             backward = 'lprevious',
             forward = 'lnext',
-        }
-        h:command_pair {
-            key = 'L',
-            backward = 'lfirst',
-            forward = 'llast',
-        }
-        h:command_pair {
-            key = '<C-l>',
-            backward = 'lpfile',
-            forward = 'lnfile',
-        }
-        h:command_pair {
-            key = 'q',
-            backward = 'cprevious',
-            forward = 'cnext',
-        }
-        h:command_pair {
-            key = 'Q',
-            backward = 'cfirst',
-            forward = 'clast',
-        }
-        h:command_pair {
-            key = '<C-q>',
-            backward = 'cpfile',
-            forward = 'cnfile',
-        }
-        h:command_pair {
-            key = 'g',
-            backward = 'tprevious',
-            forward = 'tnext',
-        }
-        h:command_pair {
-            key = 'G',
-            backward = 'tfirst',
-            forward = 'tlast',
-        }
-        h:command_pair {
-            key = '<C-t>',
-            backward = 'ptprevious',
-            forward = 'ptnext',
-        }
-        h:unified_function {
-          key = 'f',
-            desc = 'jump to the {previous|next} file in the directory tree',
+          }
+          h:command_pair {
+              key = 'L',
+              backward = 'lfirst',
+              forward = 'llast',
+          }
+          h:command_pair {
+              key = '<C-l>',
+              backward = 'lpfile',
+              forward = 'lnfile',
+          }
+          h:command_pair {
+              key = 'q',
+              backward = 'cprevious',
+              forward = 'cnext',
+          }
+          h:command_pair {
+              key = 'Q',
+              backward = 'cfirst',
+              forward = 'clast',
+          }
+          h:command_pair {
+              key = '<C-q>',
+              backward = 'cpfile',
+              forward = 'cnfile',
+          }
+          h:command_pair {
+              key = 'g',
+              backward = 'tprevious',
+              forward = 'tnext',
+          }
+          h:command_pair {
+              key = 'G',
+              backward = 'tfirst',
+              forward = 'tlast',
+          }
+          h:command_pair {
+              key = '<C-t>',
+              backward = 'ptprevious',
+              forward = 'ptnext',
+          }
+          h:unified_function {
+            key = 'f',
+              desc = 'jump to the {previous|next} file in the directory tree',
+              fun = function(direction)
+                local win_info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1] or {}
+                if win_info.quickfix == 1 then
+                  local cmd
+                  if win_info.loclist == 1 then
+                    if direction == 'backward' then
+                      cmd = 'lolder'
+                    else
+                      cmd = 'lnewer'
+                    end
+                  else
+                    if direction == 'backward' then
+                      cmd = 'colder'
+                    else
+                      cmd = 'cnewer'
+                    end
+                  end
+                  local ok, err = pcall(vim.cmd, {
+                    cmd = cmd,
+                    count = vim.v.count1,
+                  })
+                  if not ok then
+                    vim.api.nvim_err_writeln(err)
+                  end
+              else
+                  local it = require'impairative.helpers'.walk_files_tree(vim.fn.expand('%'), direction == 'backward')
+                  local path
+                  path = it:nth(vim.v.count1)
+                  if path then
+                    require'impairative.util'.jump_to{filename = path}
+                  end
+                end
+              end,
+          }
+          h:jump_in_buf {
+            key = 'n',
+            desc = 'jump to the {previous|next} SCM conflict marker or diff/path hunk',
+            extreme = {
+              key = 'N',
+              desc = 'jump to the {first|last} SCM conflict marker or diff/path hunk',
+            },
+            fun = require'impairative.helpers'.conflict_marker_locations,
+          }
+          h:unified_function {
+            key = '<Space>',
+            desc = 'add blank line(s) {above|below} the current line',
             fun = function(direction)
-              local win_info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1] or {}
-              if win_info.quickfix == 1 then
-                local cmd
-                if win_info.loclist == 1 then
-                  if direction == 'backward' then
-                    cmd = 'lolder'
-                  else
-                    cmd = 'lnewer'
-                  end
-                else
-                  if direction == 'backward' then
-                    cmd = 'colder'
-                  else
-                    cmd = 'cnewer'
-                  end
-                end
-                local ok, err = pcall(vim.cmd, {
-                  cmd = cmd,
-                  count = vim.v.count1,
-                })
-                if not ok then
-                  vim.api.nvim_err_writeln(err)
-                end
-            else
-                local it = require'impairative.helpers'.walk_files_tree(vim.fn.expand('%'), direction == 'backward')
-                local path
-                path = it:nth(vim.v.count1)
-                if path then
-                  require'impairative.util'.jump_to{filename = path}
-                end
+              local line_number = vim.api.nvim_win_get_cursor(0)[1]
+              if direction == 'backward' then
+                line_number = line_number - 1
               end
+              local lines = vim.fn['repeat']({''}, vim.v.count1)
+              vim.api.nvim_buf_set_lines(0, line_number, line_number, true, lines)
             end,
-        }
-        h:jump_in_buf {
-          key = 'n',
-          desc = 'jump to the {previous|next} SCM conflict marker or diff/path hunk',
-          extreme = {
-            key = 'N',
-            desc = 'jump to the {first|last} SCM conflict marker or diff/path hunk',
-          },
-          fun = require'impairative.helpers'.conflict_marker_locations,
-        }
-        h:unified_function {
-          key = '<Space>',
-          desc = 'add blank line(s) {above|below} the current line',
-          fun = function(direction)
-            local line_number = vim.api.nvim_win_get_cursor(0)[1]
-            if direction == 'backward' then
-              line_number = line_number - 1
-            end
-            local lines = vim.fn['repeat']({''}, vim.v.count1)
-            vim.api.nvim_buf_set_lines(0, line_number, line_number, true, lines)
-          end,
-        }
-        h:range_manipulation {
-          key = 'e',
-          line_key = true,
-          desc = 'exchange the line(s) with [count] lines {above|below} it',
-          fun = function(args)
-            local target
-            if args.direction == 'backward' then
-              target = args.start_line - args.count1 - 1
-            else
-              target = args.end_line + args.count1
-            end
-            vim.cmd {
-              cmd = 'move',
-              range = {args.start_line, args.end_line},
-              args = {target},
-            }
-            end,
-        }
-        h:text_manipulation {
-          key = 'u',
-          line_key = true,
-          desc = '{encode|decode} URL',
-          backward = require'impairative.helpers'.encode_url,
-          forward = require'impairative.helpers'.decode_url,
-        }
-        h:text_manipulation {
-          key = 'x',
-          line_key = true,
-          desc = '{encode|decode} XML',
-          backward = require'impairative.helpers'.encode_xml,
-          forward = require'impairative.helpers'.decode_xml,
-        }
-        h:text_manipulation {
-          key = 'y',
-          line_key = true,
-          desc = '{escape|unescape} strings (C escape rules)',
-          backward = require'impairative.helpers'.encode_string,
-          forward = require'impairative.helpers'.decode_string,
-        }
-        h:text_manipulation {
-          key = 'C',
-          line_key = true,
-          desc = '{escape|unescape} strings (C escape rules)',
-          backward = require'impairative.helpers'.encode_string,
-          forward = require'impairative.helpers'.decode_string,
-        }
+          }
+          h:range_manipulation {
+            key = 'e',
+            line_key = true,
+            desc = 'exchange the line(s) with [count] lines {above|below} it',
+            fun = function(args)
+              local target
+              if args.direction == 'backward' then
+                target = args.start_line - args.count1 - 1
+              else
+                target = args.end_line + args.count1
+              end
+              vim.cmd {
+                cmd = 'move',
+                range = {args.start_line, args.end_line},
+                args = {target},
+              }
+              end,
+          }
+          h:text_manipulation {
+            key = 'u',
+            line_key = true,
+            desc = '{encode|decode} URL',
+            backward = require'impairative.helpers'.encode_url,
+            forward = require'impairative.helpers'.decode_url,
+          }
+          h:text_manipulation {
+            key = 'x',
+            line_key = true,
+            desc = '{encode|decode} XML',
+            backward = require'impairative.helpers'.encode_xml,
+            forward = require'impairative.helpers'.decode_xml,
+          }
+          h:text_manipulation {
+            key = 'y',
+            line_key = true,
+            desc = '{escape|unescape} strings (C escape rules)',
+            backward = require'impairative.helpers'.encode_string,
+            forward = require'impairative.helpers'.decode_string,
+          }
+          h:text_manipulation {
+            key = 'C',
+            line_key = true,
+            desc = '{escape|unescape} strings (C escape rules)',
+            backward = require'impairative.helpers'.encode_string,
+            forward = require'impairative.helpers'.decode_string,
+          }
         end,
       }
     end,
