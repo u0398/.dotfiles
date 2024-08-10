0c7e30857617f5901c889b2cc575367d8f30c7f1
u0398 <ux0398@gmail.com>
2024-08-09 21:50:36 -0700

    move descriptions to key mappings

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -3,35 +3,29 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  -- opts_extend = { 'spec' },
   opts = {
     defaults = {},
     spec = {
       {
-        { '<leader>f', group = 'Find' },
-        { '<leader>l', group = 'LSP' },
+        { '<leader>f',     group = 'Find' },
+        { '<leader>l',     group = 'LSP' },
         { '<leader><tab>', group = 'Tabs' },
-        { '<leader>s', group = 'Sessions' },
-        { '<leader>r', group = 'Registers' },
-        { '<leader>%', desc = 'Set cwd to file directory' },
-        { '<leader>o', desc = 'New line below' },
-        { '<leader>O', desc = 'New line above' },
-        { '<leader>m', desc = 'Messages' },
-        { '<leader>M', desc = 'Clear messages' },
+        { '<leader>s',     group = 'Sessions' },
+        { '<leader>r',     group = 'Registers' },
       },
     },
     delay = 1000,
     keys = {
-      scroll_down = "<a-d>", -- binding to scroll down inside the popup
-      scroll_up = "<a-u>", -- binding to scroll up inside the popup
+      scroll_down = '<a-d>', -- binding to scroll down inside the popup
+      scroll_up =   '<a-u>', -- binding to scroll up inside the popup
     },
   },
   keys = {
     { '<leader><space>',
       function()
-        require("which-key").show({ global = false })
+        require'which-key'.show({ global = false })
       end,
-      desc = "Buffer Local Keymaps (which-key)",
+      desc = 'Buffer Local Keymaps (which-key)',
     },
   },
   config = function(_, opts)
