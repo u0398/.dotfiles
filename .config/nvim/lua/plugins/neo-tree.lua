4e259d5b5621ce7ae33facf2e8d727cdb915a14f
u0398 <ux0398@gmail.com>
2024-08-12 12:11:31 -0700

    formatting

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -2,33 +2,33 @@
 -- browse the file system and other tree like structures in whatever style suits you
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
+    'nvim-neo-tree/neo-tree.nvim',
     keys = {
-      { "<leader>fe",
+      { '<leader>fe',
         function()
-          require("neo-tree.command").execute({ toggle = true })
+          require'neo-tree.command'.execute( { toggle = true } )
         end,
-        desc = "Explorer NeoTree (Root Dir)",
+        desc = 'Explorer NeoTree (Root Dir)',
       },
       {
-        "<leader>fE",
+        '<leader>fE',
         function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+          require'neo-tree.command'.execute( { toggle = true, dir = vim.uv.cwd() } )
         end,
-        desc = "Explorer NeoTree (cwd)",
+        desc = 'Explorer NeoTree (cwd)',
       },
     },
     deactivate = function()
-      vim.cmd([[Neotree close]])
+      vim.cmd( [[Neotree close]] )
     end,
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
     },
   opts = {
-    sources = { "filesystem", "buffers", "git_status" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    sources = { 'filesystem', 'buffers', 'git_status' },
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
     close_if_last_window = true,
     filesystem = {
       bind_to_cwd = false,
@@ -37,16 +37,16 @@ return {
     },
     window = {
       mappings = {
-        ["l"] = "open",
-        ["h"] = "close_node",
-        ["<space>"] = "none",
-        ["Y"] = {
+        ['l'] = 'open',
+        ['h'] = 'close_node',
+        ['<space>'] = 'none',
+        ['Y'] = {
           function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
+            vim.fn.setreg( '+', path, 'c' )
           end,
-          desc = "Copy Path to Clipboard",
+          desc = 'Copy Path to Clipboard',
         },
         -- ["O"] = {
         --   function(state)
@@ -54,31 +54,31 @@ return {
         --   end,
         --   desc = "Open with System Application",
         -- },
-        ["P"] = { "toggle_preview", config = { use_float = false } },
+        ['P'] = { 'toggle_preview', config = { use_float = false } },
       },
     },
     default_component_configs = {
       indent = {
         with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
+        expander_collapsed = '',
+        expander_expanded = '',
+        expander_highlight = 'NeoTreeExpander',
       },
       git_status = {
         symbols = {
-          unstaged = "󰄱",
-          staged = "󰱒",
+          unstaged = '󰄱',
+          staged = '󰱒',
         },
       },
     },
   },
   config = function(_, opts)
-    require("neo-tree").setup(opts)
-    vim.api.nvim_create_autocmd("TermClose", {
-      pattern = "*lazygit",
+    require'neo-tree'.setup(opts)
+    vim.api.nvim_create_autocmd('TermClose', {
+      pattern = '*lazygit',
       callback = function()
-        if package.loaded["neo-tree.sources.git_status"] then
-          require("neo-tree.sources.git_status").refresh()
+        if package.loaded[ 'neo-tree.sources.git_status' ] then
+          require'neo-tree.sources.git_status'.refresh()
         end
       end,
     })
