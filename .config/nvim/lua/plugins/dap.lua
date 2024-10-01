36bb1a89f8345881f27a5b37810d935da2404f1b
u0398 <ux0398@gmail.com>
2024-09-30 20:40:24 -0700

    improved dap

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -13,13 +13,24 @@ return {
             host = '127.0.0.1',
             port = 8086,
           }
-            dap.run = function(c)
-              adapter.port = c.port
-              adapter.host = c.host
-            end
-            require'osv'.run_this()
+          dap.run = function(c)
+            adapter.port = c.port
+            adapter.host = c.host
+          end
+          require'osv'.run_this()
           callback(adapter)
         end
+        dap.adapters.codelldb = {
+          type = 'server',
+          port = "${port}",
+          executable = {
+            command = os.getenv('HOME') .. '/.local/share/nvim/mason/packages/codelldb/codelldb',
+            args = {"--port", "${port}"},
+
+            -- On windows you may have to uncomment this:
+            -- detached = false,
+          }
+        }
         dap.configurations.lua = {
           {
             type = 'nlua',
@@ -34,7 +45,27 @@ return {
             port = 8086,
           },
         }
+        dap.configurations.cpp = {
+          {
+            name = "Launch file",
+            type = "codelldb",
+            request = "launch",
+            program = function()
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+          },
+        }
+        dap.configurations.c = dap.configurations.cpp
+        dap.configurations.rust = dap.configurations.cpp
       end,
+    },
+    { 'rust-lang/rust.vim',
+      ft = 'rust',
+      init = function ()
+        vim.g.rustfmt_autosave = 1
+      end
     },
     { 'Saecki/crates.nvim',
       event = { 'BufRead Cargo.toml' },
--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -36,6 +36,18 @@ return {
         },
       },
     },
+    dap = {
+      adapter = function()
+        local mason_registry = require'mason-registry'
+        local codelldb = mason_registry.get_package'codelldb'
+        local extension_path = codelldb:get_install_path() .. '/extension'
+        local codelldb_path = extension_path .. 'adapter/codelldb'
+        local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
+        local cfg = require'rustaceanvim.config'
+
+        return cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+      end,
+    },
   },
   config = function(_, opts)
     vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
