bbfa9dc9d6d4f5d88f627726b58ea651bbfc6303
u0398 <ux0398@gmail.com>
2025-05-03 15:53:31 -0700

    include go/bin path checking

--- .config/profile.sh
+++ .config/profile.sh
@@ -17,6 +17,11 @@ if [ -d "$HOME/.local/bin" ] ; then
   PATH="$HOME/.local/bin:$PATH"
 fi
 
+# set PATH so it includes user's go/bin if it exists
+if [ -d "$HOME/go/bin" ] ; then
+  PATH="$HOME/go/bin:$PATH"
+fi
+
 # Load cargo if it exists
 if [ -d "$HOME/.cargo" ] ; then
   . "$HOME/.cargo/env"
